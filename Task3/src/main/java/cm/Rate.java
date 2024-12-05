472b1dac7ecbd0f3c808d66b0733148eabbab590
Seán Rourke <seanrourke3@gmail.com>
2024-12-05 15:17:01 +0000

    Fixed normal rate can be greater than 10 bug

--- Task3/src/main/java/cm/Rate.java
+++ Task3/src/main/java/cm/Rate.java
@@ -21,6 +21,9 @@ public class Rate {
         if (normalRate.compareTo(BigDecimal.ZERO) < 0 || reducedRate.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException("A rate cannot be negative");
         }
+        if (normalRate.compareTo(new BigDecimal(10)) > 0) {
+            throw new IllegalArgumentException("The normal rate cannot be greater than ten");
+        }
         if (normalRate.compareTo(reducedRate) < 0) {
             throw new IllegalArgumentException("The normal rate cannot be less or equal to the reduced rate");
         }
